c65fc19a285216724a4327284e6c108abd457a7f
Lukas <115779773+lukas-greve-sonarsource@users.noreply.github.com>
2024-01-24 16:15:03 +0100

    Update secondtest.php

--- secondtest.php
+++ secondtest.php
@@ -1,4 +1,13 @@
+
+class AuthenticationHandler {
+
+    public mysqli $conn;
+
 function insertIntoDb($param) {
     $value = $_GET[$param];
     $sql = "INSERT INTO db.table (`column`) VALUES ($value)";
     $this->db->query($sql);
+
+        }
+
+    }
