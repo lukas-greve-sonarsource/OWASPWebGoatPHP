3d7a6a460734c9f1e5aa784d475c652c0d23e8dc
Lukas <115779773+lukas-greve-sonarsource@users.noreply.github.com>
2024-01-24 17:38:36 +0100

    Update testvulnsql.php

--- testvulnsql.php
+++ testvulnsql.php
@@ -1,5 +1,8 @@
-<?php
-$value = $_GET['param'];
-$sql =  "INSERT INTO db.table (`column`) VALUES ($value)";
-$this->db->query($sql);
-?>
+function insertData($value) {
+    // FILEPATH: /home/lukas.greve/Code/OWASPWebGoatPHP/phpvuln.php
+    $value = $_GET['param'];
+    
+    // FILEPATH: /home/lukas.greve/Code/OWASPWebGoatPHP/phpvuln.php
+    $sql =  "INSERT INTO db.table (`column`) VALUES ($value)";
+    $this->db->query($sql);
+}
